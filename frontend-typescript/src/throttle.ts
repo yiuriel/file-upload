c96b9e8e8370eb914d28d94a75fa0da89a779380
yiuriel <sebastian.murawczik@gmail.com>
2024-12-06 08:52:17 +0100

    add files

---
+++ frontend-typescript/src/throttle.ts
@@ -0,0 +1,22 @@
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let lastFunc: ReturnType<typeof setTimeout>;
+  let lastRan: number;
+
+  return function (...args: Parameters<T>) {
+    if (!lastRan) {
+      func(...args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func(...args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
