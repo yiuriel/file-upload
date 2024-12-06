5058c359965c11e1f9b6c219f237d9f364bdfaff
yiuriel <sebastian.murawczik@gmail.com>
2024-12-06 08:52:12 +0100

    beautiful css upload download animation

--- frontend-typescript/src/main.ts
+++ frontend-typescript/src/main.ts
@@ -1,26 +1,69 @@
 import "./style.css";
+import { throttle } from "./throttle";
 
 async function main() {
-  const blob = new Blob([new Uint8Array(10 * 1024 * 256)]); // any Blob, including a File
-  const uploadProgress =
-    document.querySelector<HTMLProgressElement>("#upload-progress");
-  const downloadProgress =
-    document.querySelector<HTMLProgressElement>("#download-progress");
+  const downloadProgressCallback = throttle((event: ProgressEvent) => {
+    const progress = event.loaded / event.total;
+
+    const downloadProgressNumber = document.querySelector<HTMLDivElement>(
+      ".download-progress-number"
+    );
+    if (downloadProgressNumber) {
+      downloadProgressNumber.innerText = `${Math.round(progress * 100)}%`;
+    }
+
+    const uploadProgress =
+      document.querySelector<HTMLDivElement>(".upload-progress");
+    if (uploadProgress) {
+      uploadProgress.style.display = "none";
+    }
+
+    const downloadProgress =
+      document.querySelector<HTMLDivElement>(".download-progress");
+    if (downloadProgress) {
+      downloadProgress.style.setProperty(
+        "--blue-color-percentage",
+        `${progress * 100}%`
+      );
+
+      downloadProgress.style.setProperty(
+        "--red-color-percentage",
+        `${progress < 1 ? progress * (100 * progress) : 100}%`
+      );
+    }
+  }, 50);
+
+  const uploadProgressCallback = throttle((event: ProgressEvent) => {
+    const progress = event.loaded / event.total;
+
+    const uploadProgressNumber = document.querySelector<HTMLDivElement>(
+      ".upload-progress-number"
+    );
+    if (uploadProgressNumber) {
+      uploadProgressNumber.innerText = `${Math.round(progress * 100)}%`;
+    }
+
+    const uploadProgress =
+      document.querySelector<HTMLDivElement>(".upload-progress");
+    if (uploadProgress) {
+      uploadProgress.style.setProperty(
+        "--blue-color-percentage",
+        `${progress * 100}%`
+      );
+
+      uploadProgress.style.setProperty(
+        "--red-color-percentage",
+        `${progress < 1 ? progress * (100 * progress) : 100}%`
+      );
+    }
+  }, 50);
+
+  const blob = new Blob([new Uint8Array(10 * 1024 * 1024)]); // any Blob, including a File
 
   const xhr = new XMLHttpRequest();
   const success = await new Promise((resolve) => {
-    xhr.upload.addEventListener("progress", (event) => {
-      if (event.lengthComputable && uploadProgress) {
-        console.log("upload progress:", event.loaded / event.total);
-        uploadProgress.value = event.loaded / event.total;
-      }
-    });
-    xhr.addEventListener("progress", (event) => {
-      if (event.lengthComputable && downloadProgress) {
-        console.log("download progress:", event.loaded / event.total);
-        downloadProgress.value = event.loaded / event.total;
-      }
-    });
+    xhr.upload.addEventListener("progress", uploadProgressCallback);
+    xhr.addEventListener("progress", downloadProgressCallback);
     xhr.addEventListener("loadend", () => {
       resolve(xhr.readyState === 4 && xhr.status === 200);
     });
